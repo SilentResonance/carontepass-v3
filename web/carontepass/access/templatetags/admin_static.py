c0f267f0399fbc365011553dc588399dd70ee578
SilentResonance <38491750+SilentResonance@users.noreply.github.com>
2021-04-16 03:41:00 +0200

    Added templatetags to avoid changes in django-admin-bootstrapped templates See issue #262 on https://github.com/django-admin-bootstrapped/django-admin-bootstrapped/issues/262

---
+++ web/carontepass/access/templatetags/admin_static.py
@@ -0,0 +1,9 @@
+# templatetags/admin_static.py
+from django.conf import settings
+from django.template import Library
+
+register = Library()
+
+from django.templatetags.static import static
+
+static = register.simple_tag(static)
